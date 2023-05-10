4a348d61a0da5f119a538ac157044623094c6921
rafael <silva.rafa@hotmail.com>
2023-05-10 17:21:30 -0300

    criado atributo de classe olhos

--- OO/pessoa.py
+++ OO/pessoa.py
@@ -1,4 +1,8 @@
+#reflexão, se o atributo for igual para todos os objetos , crie um atributo de classe (como aqui no exercicios "Olhos"). Agora se
+# o atributo for variar o valor crie um atributo de instancia , ou seja dentro da função
+
 class Pessoa:
+    olhos = 2 # atributo de classe
     # "uma quantidade variavel de filhos => '*filhos'"
     def __init__(self, *filhos, nome=None, idade=35):  # abaixo acompanhados do self são os atributos
         self.idade = idade
@@ -20,5 +24,12 @@ if __name__ == '__main__':
         print(filho.nome)
     luciano.sobrenome = 'Ramalho'
     del luciano.filhos# função del esta retirando o atributo 'filhos' de luciano
+    luciano.olhos = 1 # aqui só altera o valor do atributo olhos apenas para objeto luciano.
+    del luciano.olhos
     print(luciano.__dict__)#o dict é um dicionario , e ira mandar imprimir tudo que esta relacionado a variavel 'luciano' neste caso.
     print(renzo.__dict__)
+    Pessoa.olhos = 3 # para alterar o valor do atributo olhos no geral temos que pegar a classe Pessoa. Pq ele é um atributo de classe
+    print(Pessoa.olhos)
+    print(luciano.olhos)#a gente pode acessar este atributo olhos tanto da classe Pessoa , quanto dos objetos luciano e renzo
+    print(renzo.olhos)
+    print(id(Pessoa.olhos), id(luciano.olhos), id(renzo.olhos))# e quando imprimir os numeros de id serão todos iguais
